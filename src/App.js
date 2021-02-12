6e753770267f88a80fe646dda4c5cf4923fa250d
Edson V Reginold <edson.reginold@surveysparrow.com>
2021-02-12 17:35:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const lm = window.LinkMink('pub_live_f9c8ZYTqgAarabIQAriU');
+  
+  const lm_data = window.location.href.includes('lmref') && lm.getReferralData() || null;
+  console.log(lm_data);
+  console.log(window.location.href);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <span>lmData: {lm_data}</span>
     </div>
   );
 }
