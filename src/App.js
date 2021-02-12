0fc9f663c368bc5febf9c233715621e996a4463b
Edson V Reginold <edson.reginold@surveysparrow.com>
2021-02-12 18:03:24 +0530

    removed lmref check from query params

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   const lm = window.LinkMink('pub_live_f9c8ZYTqgAarabIQAriU');
   
-  const lm_data = window.location.href.includes('lmref') && lm.getReferralData() || null;
+  const lm_data = lm.getReferralData() ;
   console.log(lm_data);
   console.log(window.location.href);
   return (
